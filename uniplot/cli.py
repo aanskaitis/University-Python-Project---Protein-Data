55fc98ecb519808a8a5f8877dcbaee8d31dc890e
b9053803 <a.anskaitis1@newcastle.ac.uk>
2019-10-14 11:17:01 +0100

    Seperated dump and list

--- uniplot/cli.py
+++ uniplot/cli.py
@@ -1,8 +1,27 @@
+import argparse
 import gzip
 from Bio import SeqIO
 
-def cli():
+def dump(args):
     handle = gzip.open("uniprot_receptor.xml.gz")
     for record in SeqIO.parse(handle, "uniprot-xml"):
         print(record)
+
+def names(args):
+    handle = gzip.open("uniprot_receptor.xml.gz")
+    for record in SeqIO.parse(handle, "uniprot-xml"):
+        print(record.name)
+
+
+def cli():
+    parser = argparse.ArgumentParser(prog="uniplot")
+
+    subparsers = parser.add_subparsers(help="Sub Command Help")
+
+    subparsers.add_parser("dump").set_defaults(func=dump)
+    subparsers.add_parser("list").set_defaults(func=names)
+
+    args = parser.parse_args()
+
+    args.func(args)
 
