8a7e429e8252544b78c04beddf783eb22ab544a8
b9053803 <a.anskaitis1@newcastle.ac.uk>
2019-10-16 10:27:43 +0100

    Average function

---
+++ uniplot/analysis.py
@@ -0,0 +1,4 @@
+
+
+def average_len(records):
+    return sum(len(record) for record in records) / len(records)
--- uniplot/cli.py
+++ uniplot/cli.py
@@ -1,7 +1,13 @@
 import argparse
 from . import parse
+from . import analysis
 
 LOC="uniprot_receptor.xml.gz"
+
+def average(args):
+    print("Average length is {}".format(
+        analysis.average_len(parse.uniprot_seqrecords(LOC))
+    ))
 
 def dump(args):
     for record in parse.uniprot_seqrecords(LOC):
@@ -20,6 +26,7 @@ def cli():
 
     subparsers.add_parser("dump").set_defaults(func=dump)
     subparsers.add_parser("list").set_defaults(func=names)
+    subparsers.add_parser("average").set_defaults(func=average)
 
     args = parser.parse_args()
 
