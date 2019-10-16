9ece181ebfd12093fc949f357295bb3da5e3495d
b9053803 <a.anskaitis1@newcastle.ac.uk>
2019-10-16 11:21:19 +0100

    Average length for the top level taxa

--- uniplot/analysis.py
+++ uniplot/analysis.py
@@ -2,3 +2,11 @@
 
 def average_len(records):
     return round(sum(len(record) for record in records) / len(records))
+
+def average_len_taxa(records):
+    record_by_taxa = {}
+    for r in records:
+        taxa = r.annotations["taxonomy"][0]
+        record_by_taxa.setdefault(taxa, []).append(r)
+
+    return {taxa:average_len(record) for (taxa, record) in record_by_taxa.items()}
