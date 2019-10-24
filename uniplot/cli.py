633ae2cb67607fffd0e0852e9f31e6df754be15d
b9053803 <a.anskaitis1@newcastle.ac.uk>
2019-10-24 11:33:58 +0100

    Location and Depth finished

--- uniplot/cli.py
+++ uniplot/cli.py
@@ -8,26 +8,27 @@ LOC="uniprot_receptor.xml.gz"
 def average(args):
     """This function prints the average length length of lists in set"""
     print("Average length is {}".format(
-        analysis.average_len(parse.uniprot_seqrecords(args.path))
+        analysis.average_len(parse.uniprot_seqrecords(LOC))
     ))
 
 def dump(args):
     """This function prints out all the records"""
-    for record in parse.uniprot_seqrecords(args.path):
+    for record in parse.uniprot_seqrecords(LOC):
         print(record)
 
 def names(args):
     """This function prints out all names of the records"""
-    for record in parse.uniprot_seqrecords(args.path):
+    for record in parse.uniprot_seqrecords(LOC):
         print(record.name)
 
 def plot_average_by_taxa(args):
-    """This function shows the bar chart of the average length of lists in set in different groups"""
-    av = analysis.average_len_taxa(parse.uniprot_seqrecords(args.path), args.depth)
+    """This function shows the bar chart of the average length of different proteins by taxa"""
+    av = analysis.average_len_taxa(parse.uniprot_seqrecords(LOC), input("Choose depth = "))
     plot.plot_bar_show(av)
 
 def plot_pie_average_by_taxa(args):
-    av = analysis.average_len_taxa(parse.uniprot_seqrecords(args.path), args.depth)
+    """This function shows the pie chart of the average length of different proteins by taxa"""
+    av = analysis.average_len_taxa(parse.uniprot_seqrecords(LOC),input("Choose depth = "))
     plot.plot_pie_show(av)
 
 
@@ -42,12 +43,12 @@ def cli():
     subparsers.add_parser("dump",help="To print out all the records").set_defaults(func=dump)
     subparsers.add_parser("list",help="To print out the names of protein sequences").set_defaults(func=names)
     subparsers.add_parser("average",help= "To print out an average length of one protein sequence").set_defaults(func=average)
-    subparsers.add_parser("plot-average-by-taxa",help="To show the bar chart of average length of proteins sequences by taxa").set_defaults(func=plot_average_by_taxa)
-    subparsers.add_parser("plot-pie-average-by-taxa",help="To show the pie chart of average length of proteins sequences by taxa").set_defaults(func=plot_pie_average_by_taxa)
-
-    ## Add arguments
-    parser.add_argument("path",type=str,help="To specify a file location")
-    parser.add_argument("depth",help="To specify depth for the bar or pie chart",default=1)
+    subparsers.add_parser("plot-average-by-taxa",help="To show the bar chart of average length of proteins sequences by taxa. "
+                                                      "You have to choose the depth of taxa, write 1 for default and 2 for more detailed.")\
+        .set_defaults(func=plot_average_by_taxa)
+    subparsers.add_parser("plot-pie-average-by-taxa",help="To show the pie chart of average length of proteins sequences by taxa. "
+                                                          "You have to choose the depth of taxa, write 1 for default and 2 for more detailed.")\
+        .set_defaults(func=plot_pie_average_by_taxa)
 
     ## Parse the command line
     args = parser.parse_args()
