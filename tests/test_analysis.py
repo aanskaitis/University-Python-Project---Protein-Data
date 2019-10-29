1d3c648cc678ebe5e94e73b992c4bed7ce53bd77
b9053803 <a.anskaitis1@newcastle.ac.uk>
2019-10-29 14:31:44 +0000

    Location is configurable, depth option and pie chart finished.

--- tests/test_analysis.py
+++ tests/test_analysis.py
@@ -10,4 +10,4 @@ TEST_UNIPROT="./resources/uniprot_sprot_small.xml.gz"
 def test_average():
     assert uniplot.analysis.average_len(
         uniplot.parse.uniprot_seqrecords(TEST_UNIPROT)
-    ) == 303
+    ) == 302.72222222222223
